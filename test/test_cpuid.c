a84e078952f922b365a6bafc07bf2c7f2635ee3a
Thomas Bunk <TBunk1989@gmail.com>
2018-03-27 11:53:08 +0200

    Change name of testfile according to CMock policy

--- test/test_cpuid.c
+++ test/test_cpuid.c
@@ -1,12 +1,22 @@
 #include "unity.h"
 #include "cpuid.h"
 
+/*
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+*/
+
 void test_GetVendorName_should_ReturnGenuineIntel(void) {
   char actual[20];
   get_vendor_name(actual);
   
   TEST_ASSERT_EQUAL_STRING("GenuineIntel", actual);
+  //TEST_ASSERT_TRUE(1);
 }
+
 
 void test_GetProcessorSignature_should_ReturnFamily6(void) {
   unsigned int family;
@@ -14,12 +24,5 @@ void test_GetProcessorSignature_should_ReturnFamily6(void) {
   family = (processor_signature >> 8) & 0xf;
 
   TEST_ASSERT_EQUAL_UINT(0x6, family);
-}
-
-int main(void) {
-  UNITY_BEGIN();
-  RUN_TEST(test_GetVendorName_should_ReturnGenuineIntel);
-  RUN_TEST(test_GetProcessorSignature_should_ReturnFamily6);
-  return UNITY_END();
 }
 
