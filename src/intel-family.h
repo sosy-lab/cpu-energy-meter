d83f63bf8fcb4c1f086c2e98fd19c1f0f994d13a
Philipp Wendler <uni@philippwendler.de>
2019-04-05 10:08:52 +0200

    remove tabs

--- src/intel-family.h
+++ src/intel-family.h
@@ -38,37 +38,37 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 * Xeons.
 */
 
-#define CPU_INTEL_SANDYBRIDGE		    0x206A0     // Family 6 Model 42 (0x2a)
+#define CPU_INTEL_SANDYBRIDGE           0x206A0     // Family 6 Model 42 (0x2a)
 #define CPU_INTEL_SANDYBRIDGE_X         0x206D0     // Family 6 Model 45 (0x2d)
 
-#define CPU_INTEL_IVYBRIDGE		        0x306A0     // Family 6 Model 58 (0x3a)
-#define CPU_INTEL_IVYBRIDGE_X		    0x306E0     // Family 6 Model 62 (0x3e)
+#define CPU_INTEL_IVYBRIDGE             0x306A0     // Family 6 Model 58 (0x3a)
+#define CPU_INTEL_IVYBRIDGE_X           0x306E0     // Family 6 Model 62 (0x3e)
 
-#define CPU_INTEL_HASWELL_CORE		    0x306C0     // Family 6 Model 60 (0x3c)
-#define CPU_INTEL_HASWELL_ULT		    0x40650     // Family 6 Model 69 (0x45)
-#define CPU_INTEL_HASWELL_GT3E		    0x40660     // Family 6 Model 70 (0x46)
+#define CPU_INTEL_HASWELL_CORE          0x306C0     // Family 6 Model 60 (0x3c)
+#define CPU_INTEL_HASWELL_ULT           0x40650     // Family 6 Model 69 (0x45)
+#define CPU_INTEL_HASWELL_GT3E          0x40660     // Family 6 Model 70 (0x46)
 #define CPU_INTEL_HASWELL_X             0x306F0     // Family 6 Model 63 (0x3f)
 
-#define CPU_INTEL_BROADWELL_CORE	    0x306D0     // Family 6 Model 61 (0x3d)
-#define CPU_INTEL_BROADWELL_GT3E	    0x40670     // Family 6 Model 71 (0x47)
-#define CPU_INTEL_BROADWELL_X		    0x406F0     // Family 6 Model 79 (0x4f)
-#define CPU_INTEL_BROADWELL_XEON_D	    0x50660     // Family 6 Model 64 (0x56)
+#define CPU_INTEL_BROADWELL_CORE        0x306D0     // Family 6 Model 61 (0x3d)
+#define CPU_INTEL_BROADWELL_GT3E        0x40670     // Family 6 Model 71 (0x47)
+#define CPU_INTEL_BROADWELL_X           0x406F0     // Family 6 Model 79 (0x4f)
+#define CPU_INTEL_BROADWELL_XEON_D      0x50660     // Family 6 Model 64 (0x56)
 
-#define CPU_INTEL_SKYLAKE_MOBILE	    0x406E0     // Family 6 Model 78 (0x4e)
-#define CPU_INTEL_SKYLAKE_DESKTOP	    0x506E0     // Family 6 Model 94 (0x5e)
+#define CPU_INTEL_SKYLAKE_MOBILE        0x406E0     // Family 6 Model 78 (0x4e)
+#define CPU_INTEL_SKYLAKE_DESKTOP       0x506E0     // Family 6 Model 94 (0x5e)
 #define CPU_INTEL_SKYLAKE_X             0x50650     // Family 6 Model 85 (0x55)
 
-#define CPU_INTEL_KABYLAKE_MOBILE	    0x806E0     // Family 6 Model 142 (0x8e)
-#define CPU_INTEL_KABYLAKE_DESKTOP	    0x906E0     // Family 6 Model 158 (0x9e)
+#define CPU_INTEL_KABYLAKE_MOBILE       0x806E0     // Family 6 Model 142 (0x8e)
+#define CPU_INTEL_KABYLAKE_DESKTOP      0x906E0     // Family 6 Model 158 (0x9e)
 
 #define CPU_INTEL_CANNONLAKE            0x60660     // Family 6 Model 102 (0x66)
 
 /* "Small Core" Processors (Atom) */
-#define CPU_INTEL_ATOM_SILVERMONT1	    0x30670     // Family 6 Model 55 (0x37) /* E3000 series, Z3600 series, Z3700 series */
-#define CPU_INTEL_ATOM_MERRIFIELD	    0x406A0     // Family 6 Model 74 (0x4a)
-#define CPU_INTEL_ATOM_AIRMONT		    0x406C0     // Family 6 Model 76 (0x4c)
-#define CPU_INTEL_ATOM_SILVERMONT2	    0x406D0     // Family 6 Model 77 (0x4d) /* C2000 series */
-#define CPU_INTEL_ATOM_MOOREFIELD	    0x506A0     // Family 6 Model 90 (0x5a)
+#define CPU_INTEL_ATOM_SILVERMONT1      0x30670     // Family 6 Model 55 (0x37) /* E3000 series, Z3600 series, Z3700 series */
+#define CPU_INTEL_ATOM_MERRIFIELD       0x406A0     // Family 6 Model 74 (0x4a)
+#define CPU_INTEL_ATOM_AIRMONT          0x406C0     // Family 6 Model 76 (0x4c)
+#define CPU_INTEL_ATOM_SILVERMONT2      0x406D0     // Family 6 Model 77 (0x4d) /* C2000 series */
+#define CPU_INTEL_ATOM_MOOREFIELD       0x506A0     // Family 6 Model 90 (0x5a)
 #define CPU_INTEL_ATOM_SILVERMONT3      0x506D0     // Family 6 Model 93 (0x5d) /* X3-C3000 series */
 #define CPU_INTEL_ATOM_GOLDMONT         0x506C0     // Family 6 Model 92 (0x5c)
 #define CPU_INTEL_ATOM_DENVERTON        0x506F0     // Family 6 Model 95 (0x5f)
