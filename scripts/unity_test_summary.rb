6d3a7d77ab386991d2930ba230b3619ee9fd0507
Philipp Wendler <uni@philippwendler.de>
2021-03-18 14:51:04 +0100

    Convert another license declaration to REUSE style

--- scripts/unity_test_summary.rb
+++ scripts/unity_test_summary.rb
@@ -1,31 +1,16 @@
-# License: The MIT License (MIT), taken from "Unity Project - A Test Framework
-#          for C" (https://github.com/ThrowTheSwitch/Unity)
-#
-# Copyright (c) 2007-14 Mike Karlesky, Mark VanderVoord, Greg Williams
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+#!/usr/bin/ruby
 
+# This file is part of CPU Energy Meter,
+# a tool for measuring energy consumption of Intel CPUs:
+# https://github.com/sosy-lab/cpu-energy-meter
+#
+# SPDX-FileCopyrightText: 2007-2014 Mike Karlesky, Mark VanderVoord, Greg Williams
+# SPDX-FileCopyrightText: 2018-2021 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: MIT
 
-# !/usr/bin/ruby
-#
-# unity_test_summary.rb
-#
+# Taken from the Unity Project - A Test Framework for C" (https://github.com/ThrowTheSwitch/Unity)
+
 require 'fileutils'
 require 'set'
 
